54ab52d254c3f9f9709ea98c647573b3bbc9948e
Johannes Erwerle <johannes.erwerle@googlemail.com>
2017-11-19 22:57:02 +0100

    fixed code, added some documentation, updated readme

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ This script is intended to be used with a resetted switch via serial.
 It was tested only with a ProCurve 2824.
 
 todo:
-- [ ] clean up the code 
-- [ ] add some documentation
-- [ ] add password setting method
+- [x] clean up the code 
+- [x] add some documentation
+- [x] add password setting method
 - [x] write example config
 - [x] requirements.txt
--- procurve-conf.py
+++ procurve-conf.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
-import time
+""" procurve conf is a script to configure HP Procurve Switches starting at
+    factory defaults via a serial interface.
 
+    Further information at https://github.com/991jo/procurve-conf
+"""
+
+from time import sleep
 from sys import stdout
-from pexpect import fdpexpect
+
 import serial
+from pexpect import fdpexpect
 
 config = "config.txt"
 device = "/dev/ttyUSB0"
+manager_pw = "managerpw"
+operator_pw = "operatorpw"
+debug_mode = True
+
+""" Assumptions made in the code:
+    - every function assumes to be in operator-mode and ready to execute a command
+    - every function leaves the cli in the operator-mode but not in configure mode
+"""
+
+def debug_msg(message):
+    if debug_mode:
+        print(message)
 
 def gen_ssh_keys(child):
-    child.expect(".*#")
+    """generates a new ssh key pair"""
     child.sendline("end")
     child.expect(".*?(?!\(.*?\))#")
     child.sendline("conf term")
@@ -19,11 +37,26 @@ def gen_ssh_keys(child):
     child.expect("Installing new RSA key", timeout=60)
     child.expect(".*\(config\)#")
 
+
 def set_passwords(child, manager_pw, operator_pw):
-    pass
+    """ sets the passwords for manager and operator user accounts """
+    child.sendline("configure terminal")
+    child.expect(".+\(config\)#")
+    child.sendline("password all")
+    child.expect(".+password.+Operator")
+    child.sendline(operator_pw)
+    child.expect(".+password.+Operator")
+    child.sendline(operator_pw)
+    child.expect(".+password.+Manager")
+    child.sendline(manager_pw)
+    child.expect(".+password.+Manager")
+    child.sendline(manager_pw)
+
 
 def wait_for_boot(child):
-    print("debug: waiting for boot")
+    """ waits for the switch to boot and handles the Speed Sense and License
+    messages """
+    debug_msg("debug: waiting for boot")
     child.send("\r")
     child.send("\r")
     pattern_list = child.compile_pattern_list(
@@ -31,27 +64,35 @@ def wait_for_boot(child):
                 "Press any key to continue",
                 ".+#"   ])
     ptrn_nr = child.expect_list(pattern_list, timeout=400)
-    print("debug: I FOUND SOMETHING")
+    debug_msg("debug: I FOUND SOMETHING")
     if ptrn_nr == 0:
-        print("debug: case 0")
+        debug_msg("debug: case 0")
+        # it seems like multiple \r are needed for this to work reliably
         child.send("\r")
         child.send("\r")
-        time.sleep(1)
+        sleep(1)
         child.send("\r")
         child.send("\r")
-        time.sleep(1)
+        sleep(1)
         child.send("\r")
         child.send("\r")
-        wait_for_boot(child)
+        wait_for_boot(child) # recursive call for the next "screen"
     elif ptrn_nr == 1:
-        print("debug: case 1")
+        debug_msg("debug: case 1")
         child.sendline("\r")
         child.expect(".#")
     else:
-        print("debug: case 2")
+        debug_msg("debug: case 2")
         return
 
+
 def apply_config(child, config):
+    """ reads the config file stored in the config and applies it line-wise.
+    You have to make shure, that your commands do not exit configure-mode and
+    do not need further input.
+    This means that "end" is not allowed, because it leaves configure mode and
+    "password" is not allowed, because it needs further user input.
+    """
     child.sendline("end")
     child.expect(".*#")
     child.sendline("configure terminal")
@@ -59,6 +100,12 @@ def apply_config(child, config):
         for line in f:
             child.sendline(line)
             child.expect(".*#")
+    child.sendline("end")
+    child.expect(".*#")
+
+def write_memory(child):
+    """ writes the running-config to startup-config aka saves the currently running
+    config."""
     child.sendline("end")
     child.expect(".*#")
     child.sendline("write memory")
@@ -72,3 +119,5 @@ child.send("\r")
 child.expect(".*#", timeout=3)
 gen_ssh_keys(child)
 apply_config(child, config)
+set_passwords(child, manager_pw, operator_pw)
+write_memory(child)
